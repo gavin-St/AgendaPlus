c05446b5ec47b4eb0e1eb1d498ced43fa35581b4
Yi Wang <48071660+yimango@users.noreply.github.com>
2021-09-18 08:58:01 -0400

    Update main_web.js

--- main_web.js
+++ main_web.js
@@ -1,5 +1,9 @@
 function show(shown, hidden1, hidden2) {
-            document.getElementById(shown).style.display = 'block'
-            document.getElementById(hidden1).style.display = 'none'
-            document.getElementById(hidden2).style.display = 'none'
+    document.getElementById(shown).style.display = 'block'
+    document.getElementById(hidden1).style.display = 'none'
+    document.getElementById(hidden2).style.display = 'none'
 }
+
+document.getElementById("button1").addEventListener("click", function(){show('page 1', 'page 2', 'page 3')});
+document.getElementById("button2").addEventListener("click", function(){show('page 2', 'page 1', 'page 3')});
+document.getElementById("button3").addEventListener("click", function(){show('page 3', 'page 2', 'page 1')});
