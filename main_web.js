5ccf01d091a99c76aab5bb0fa7e16c41bb922b25
Yi Wang <48071660+yimango@users.noreply.github.com>
2021-09-18 08:26:49 -0400

    Update main_web.js

--- main_web.js
+++ main_web.js
@@ -1,1 +1,5 @@
-
+function show(shown, hidden1, hidden2) {
+            document.getElementById(shown).style.display = 'block'
+            document.getElementById(hidden1).style.display = 'none'
+            document.getElementById(hidden2).style.display = 'none'
+}
