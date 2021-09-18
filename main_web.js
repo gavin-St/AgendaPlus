0abe8bbfaba05785d54bc3ffea482e9e50dc5e48
Gavin Song <86136270+gavin-St@users.noreply.github.com>
2021-09-18 11:30:40 -0400

    Update main_web.js

--- main_web.js
+++ main_web.js
@@ -4,6 +4,15 @@ function show(shown, hidden1, hidden2) {
     document.getElementById(hidden2).style.display = 'none'
 }
 
-document.getElementById("button1").addEventListener("click", function(){show('page 1', 'page 2', 'page 3')});
-document.getElementById("button2").addEventListener("click", function(){show('page 2', 'page 1', 'page 3')});
-document.getElementById("button3").addEventListener("click", function(){show('page 3', 'page 2', 'page 1')});
+document.getElementById("button1").addEventListener("click", function(){
+    show('page 1', 'page 2', 'page 3')
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+    show('page 2', 'page 1', 'page 3') 
+    timeline();
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+    show('page 3', 'page 2', 'page 1')
+});
