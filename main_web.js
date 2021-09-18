d07e47a5b3877a3adc1e3861ae51f713d28ed353
DripSoup <58060581+DripSoup@users.noreply.github.com>
2021-09-18 13:48:40 -0400

    Update main_web.js

--- main_web.js
+++ main_web.js
@@ -1,3 +1,4 @@
+var currentPage = 1;
 function show(shown, hidden1, hidden2) {
     document.getElementById(shown).style.display = 'block'
     document.getElementById(hidden1).style.display = 'none'
@@ -6,13 +7,24 @@ function show(shown, hidden1, hidden2) {
 
 document.getElementById("button1").addEventListener("click", function(){
     show('page 1', 'page 2', 'page 3')
+    document.getElementById("button" + String(currentPage)).style.opacity = 1;
+    currentPage = 1;
+    document.getElementById("button" + String(currentPage)).style.opacity = 0.6;
+
 });
 
 document.getElementById("button2").addEventListener("click", function(){
     show('page 2', 'page 1', 'page 3') 
     timeline();
+    document.getElementById("button" + String(currentPage)).style.opacity = 1;
+    currentPage = 2;
+    document.getElementById("button" + String(currentPage)).style.opacity = 0.6;
+    
 });
 
 document.getElementById("button3").addEventListener("click", function(){
     show('page 3', 'page 2', 'page 1')
+    document.getElementById("button" + String(currentPage)).style.opacity = 1;
+    currentPage = 3;
+    document.getElementById("button" + String(currentPage)).style.opacity = 0.6;
 });
